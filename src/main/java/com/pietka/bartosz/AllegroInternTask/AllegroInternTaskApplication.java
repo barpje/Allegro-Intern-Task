3426ba1ef739dac737ac00556df6f87f541b4688
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2021-04-20 19:12:15 +0200

    init spring boot

---
+++ src/main/java/com/pietka/bartosz/AllegroInternTask/AllegroInternTaskApplication.java
@@ -0,0 +1,13 @@
+package com.pietka.bartosz.AllegroInternTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroInternTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroInternTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pietka/bartosz/AllegroInternTask/AllegroInternTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pietka.bartosz.AllegroInternTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroInternTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
