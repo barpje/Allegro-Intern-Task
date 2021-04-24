a7076962b6512c43af35c309337ff58a7803919d
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2021-04-24 19:46:22 +0200

    add no args constructor

--- src/main/java/com/pietka/bartosz/AllegroInternTask/models/UserData.java
+++ src/main/java/com/pietka/bartosz/AllegroInternTask/models/UserData.java
@@ -2,9 +2,11 @@ package com.pietka.bartosz.AllegroInternTask.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class UserData {
