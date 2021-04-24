3a362283b5234234731aefd9df7d3c641587d6e7
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2021-04-24 19:46:56 +0200

    remove unused imports

--- src/main/java/com/pietka/bartosz/AllegroInternTask/utils/GitHubUtil.java
+++ src/main/java/com/pietka/bartosz/AllegroInternTask/utils/GitHubUtil.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public final class GitHubUtil {
 
